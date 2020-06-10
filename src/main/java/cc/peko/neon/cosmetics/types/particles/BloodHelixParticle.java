01f1e72d51046c6aec556f4711b78fc008d54e50
Ryan <ryan@c0an.club>
2020-06-10 14:52:19 +0100

    Lets try this

--- src/main/java/cc/peko/neon/cosmetics/types/particles/BloodHelixParticle.java
+++ src/main/java/cc/peko/neon/cosmetics/types/particles/BloodHelixParticle.java
@@ -3,18 +3,17 @@ package cc.peko.neon.cosmetics.types.particles;
 import cc.peko.neon.Neon;
 import cc.peko.neon.cosmetics.Cosmetic;
 import cc.peko.neon.cosmetics.CosmeticType;
+import net.minecraft.server.v1_7_R4.PacketPlayOutWorldParticles;
 import org.bukkit.Bukkit;
-import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
+import rip.protocol.plib.util.Reflection;
 
 import java.util.*;
-
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
 
 public class BloodHelixParticle extends Cosmetic {
 
@@ -63,11 +62,22 @@ public class BloodHelixParticle extends Cosmetic {
                 Location location1 = player.getLocation();
                 for (double t = 0; t <= 2 * Math.PI; t = t + Math.PI / 16) {
                     for (double i = 0; i <= 1; i = i + 1) {
-                        x = 0.4 * (2 * Math.PI - t) * 0.5 * cos(t + phi + i * Math.PI);
+                        x = 0.4 * (2 * Math.PI - t) * 0.5 * Math.cos(t + phi + i * Math.PI);
                         y = 0.5 * t;
-                        z = 0.4 * (2 * Math.PI - t) * 0.5 * sin(t + phi + i * Math.PI);
+                        z = 0.4 * (2 * Math.PI - t) * 0.5 * Math.sin(t + phi + i * Math.PI);
                         location1.add(x, y, z);
-                        player.getWorld().playEffect(location1, Effect.COLOURED_DUST, 1);
+
+                        PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles("reddust",
+                                        (float) location1.getX() ,
+                                        (float) location1.getY(),
+                                        (float) location1.getZ(),
+                                        255, 0, 0,
+                                        1, 0);
+
+                        player.getNearbyEntities(50, 50, 50).stream().filter(entity -> entity instanceof CraftPlayer && player.canSee((Player) entity)).map(entity -> (CraftPlayer)entity).forEach(craftPlayer -> {
+                            craftPlayer.getHandle().playerConnection.sendPacket(packet);
+                        });
+
                         location1.subtract(x, y, z);
                     }
 
@@ -75,7 +85,7 @@ public class BloodHelixParticle extends Cosmetic {
 
             }
         };
-        runnable.runTaskTimer(Neon.getInstance(), 0, 1);
+        runnable.runTaskTimer(Neon.getInstance(), 0, 2);
         ringMap.put(player.getUniqueId(), runnable.getTaskId());
     }
 
